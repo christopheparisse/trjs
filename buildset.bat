72fa8a4c905d34ec15bd6d7fddc4dbda6eef3d2f
Christophe Parisse <christopheparisse@gmail.com>
2018-02-05 22:27:59 +0100

    version 0.6.2

--- buildset.bat
+++
@@ -1,7 +0,0 @@
-set npm_config_target=1.4.3
-set npm_config_arch=x64
-set npm_config_target_arch=x64
-set npm_config_disturl=https://atom.io/download/electron
-set npm_config_runtime=electron
-set npm_config_build_from_source=true
-npm install
--- buildwin.bat
+++ buildwin.bat
@@ -6,3 +6,6 @@ cd \devlopt\builds\trjs-x86
 cmd /c electron-packager app --platform=win32 --asar=true --arch=ia32 --overwrite --icon=app\style\trjs.ico
 md Trjs-win32-ia32\resources\tools
 xcopy /s/y/c tools\*.* Trjs-win32-ia32\resources\tools
+cd \devlopt\trjs
+"C:\Program Files (x86)\NSIS\Bin\makensis.exe" transcriberjs-x64.nsi
+"C:\Program Files (x86)\NSIS\Bin\makensis.exe" transcriberjs-x86.nsi
