440c4fa9df1ae4aa82d7c2fcd415692cdaf47261
Christophe Parisse <christopheparisse@gmail.com>
2018-02-28 23:49:59 +0100

    version 0.6.4

--- majpgmwin.sh
+++ majpgmwin.sh
@@ -1,2 +1,2 @@
-scp trjs-v0.6.3-x64.exe parisse@ct3.ortolang.fr:/applis/download/
-scp trjs-v0.6.3-x86.exe parisse@ct3.ortolang.fr:/applis/download/
+scp trjs-v0.6.4-x64.exe parisse@ct3.ortolang.fr:/applis/download/
+scp trjs-v0.6.4-x86.exe parisse@ct3.ortolang.fr:/applis/download/
---
+++ ver.sh
@@ -0,0 +1,5 @@
+sed -e 's/__version__/0.6.4/g' majpgmct3.shmodel > majpgmct3.sh
+sed -e 's/__version__/0.6.4/g' majpgmwin.shmodel > majpgmwin.sh
+sed -e 's/__version__/0.6.4/g' transcriberjs-x64.nsimodel > transcriberjs-x64.nsi
+sed -e 's/__version__/0.6.4/g' transcriberjs-x86.nsimodel > transcriberjs-x86.nsi
+sed -e 's/__version__/0.6.4/g' maj.shmodel > maj.sh
