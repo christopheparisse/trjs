5de9e2bb4e26a3019e1d5e615411d98e3e70665f
Christophe Parisse <christopheparisse@gmail.com>
2018-07-31 00:41:53 +0200

    version 0.6.7

--- systemcall/fsio.js
+++ systemcall/fsio.js
@@ -432,7 +432,7 @@ fsio.exportMediaSubt = function(args, doneFunction, failFunction) {
         // if (version.debug(__filename))
         //console.log('ecriture de ' + tempfn);
         fs.writeFileSync(tempfn, args['subtitles']);
-        var output = path.basename(args['media'], path.extname(args['media'])) + '-subtitle.mp4';
+        var output = path.dirname(args['media']) + '/x' + path.basename(args['media'], path.extname(args['media'])) + '-subtitle.mp4';
         return medialibrary.burnSubtitles(args['media'], output, tempfn, 2, true, parseInt(args['tmin']), parseInt(args['tmax']), 'electron', args['box'],
             function (err, mess) {
                 if (!err)
