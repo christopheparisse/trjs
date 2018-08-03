528ffbd0a76d88670fb6d4c97d7f0eb41941ed41
Christophe Parisse <christopheparisse@gmail.com>
2018-08-03 10:15:13 +0200

    version 0.6.8

--- majpgmwin.sh
+++ majpgmwin.sh
@@ -1,2 +1,2 @@
-scp trjs-v0.6.7-x64.exe parisse@ct3.ortolang.fr:/applis/download/
-scp trjs-v0.6.7-x86.exe parisse@ct3.ortolang.fr:/applis/download/
+scp trjs-v0.6.8-x64.exe parisse@ct3.ortolang.fr:/applis/download/
+scp trjs-v0.6.8-x86.exe parisse@ct3.ortolang.fr:/applis/download/
--- ver.sh
+++ ver.sh
@@ -1,6 +1,6 @@
-sed -e 's/__version__/0.6.7/g' majpgmct3.shmodel > majpgmct3.sh
-sed -e 's/__version__/0.6.7/g' majpgmwin.shmodel > majpgmwin.sh
-sed -e 's/__version__/0.6.7/g' transcriberjs-x64.nsimodel > transcriberjs-x64.nsi
-sed -e 's/__version__/0.6.7/g' transcriberjs-x86.nsimodel > transcriberjs-x86.nsi
-sed -e 's/__version__/0.6.7/g' maj.shmodel > maj.sh
-sed -e 's/__version__/0.6.7/g' maj.batmodel > maj.bat
+sed -e 's/__version__/0.6.8/g' majpgmct3.shmodel > majpgmct3.sh
+sed -e 's/__version__/0.6.8/g' majpgmwin.shmodel > majpgmwin.sh
+sed -e 's/__version__/0.6.8/g' transcriberjs-x64.nsimodel > transcriberjs-x64.nsi
+sed -e 's/__version__/0.6.8/g' transcriberjs-x86.nsimodel > transcriberjs-x86.nsi
+sed -e 's/__version__/0.6.8/g' maj.shmodel > maj.sh
+sed -e 's/__version__/0.6.8/g' maj.batmodel > maj.bat
