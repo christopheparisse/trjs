97d8c0aeef0bafc5774f3fd12543cb0086aacde4
Christophe Parisse <christopheparisse@gmail.com>
2018-12-30 00:58:43 +0100

    version 0.7.4a

--- maj.bat
+++ maj.bat
@@ -1,3 +1,3 @@
 git add . 
-git commit -m "version 0.7.4"
+git commit -m "version 0.7.4a"
 git push origin master
--- majpgmwin.sh
+++ majpgmwin.sh
@@ -1,2 +1,2 @@
-scp trjs-v0.7.4-x64.exe parisse@ct3.ortolang.fr:/applis/download/
-scp trjs-v0.7.4-x86.exe parisse@ct3.ortolang.fr:/applis/download/
+scp trjs-v0.7.4a-x64.exe parisse@ct3.ortolang.fr:/applis/download/
+scp trjs-v0.7.4a-x86.exe parisse@ct3.ortolang.fr:/applis/download/
