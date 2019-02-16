749849b7fce376f2f60fe8dab64933e3fd54640e
Christophe Parisse <christopheparisse@gmail.com>
2019-02-16 22:54:55 +0100

    version 0.7.4b

--- majpgmct3.sh
+++ majpgmct3.sh
@@ -9,7 +9,7 @@ cp trjs-v0.7.4b-macos.zip ~/ownCloud/betatrjs
 zip -r trjs-v0.7.4b.zip trjs-zip
 scp trjs-v0.7.4b.zip parisse@ct3.ortolang.fr:/applis/download/
 cp trjs-v0.7.4b.zip ~/ownCloud/betatrjs
-cd /devlopt/prod-trjs-v0.7.4b
+cd /devlopt/prod-trjs
 scp doc/trjs_pp.html parisse@ct3.ortolang.fr:/applis/trjs/documentation/
 scp doc/trjs_doc.html parisse@ct3.ortolang.fr:/applis/trjs/documentation/
 #scp trjs-v0.7.4b-x64.exe parisse@ct3.ortolang.fr:/applis/download/
